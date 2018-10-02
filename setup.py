5c70dcb6649404af798b5492e0f4b47aa3594674
Axel Bock <mr.axel.bock@gmail.com>
2018-10-02 15:52:37 +0200

    update release process, fix setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: Taken from here: https://github.com/kennethreitz/setup.py
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
 import io
+import os
+import sys
+from shutil import rmtree
 
-from setuptools import setup
+from setuptools import find_packages, setup, Command
 
-
-BASE_URL = "https://github.com/CognotektGmbH/okta-cli"
+# Package meta-data.
 VERSION = "1.0.1"
 
-#long_description = (
-#    io.open('DESCRIPTION.rst', encoding='utf-8').read() + '\n'
-#)
+NAME = 'okta-cli'
+DESCRIPTION = 'An Okta command line interface for scripting and quickly performing routine tasks'
+URL = 'https://github.com/flypenguin/okta-cli'
+EMAIL = 'mr.axel.bock@gmail.com'
+AUTHOR = 'Axel Bock'
+REQUIRES_PYTHON = '>=3.5.0'
 
+REQUIRED = [
+    "appdirs", "click", "dotted", "requests",
+]
+
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags ...')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="okta-cli",
-    version=VERSION,
-    description="An Okta CLI tool to perform routine tasks more quickly. "
-                "Work in progress :) . "
-                "More information available on the project's GitHub page.",
-    #long_description=long_description,
-    url=BASE_URL,
-    download_url=BASE_URL + "/tarball/{}".format(VERSION),
-    py_modules=[],
-    install_requires=[
-        "appdirs",
-        "click",
-        "requests",
-        "dotted",
-    ],
-    entry_points="""
-        [console_scripts]
-        okta-cli=oktacli:cli
-    """
+        name=NAME,
+        version=about['__version__'],
+        description=DESCRIPTION,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author=AUTHOR,
+        author_email=EMAIL,
+        python_requires=REQUIRES_PYTHON,
+        url=URL,
+        packages=find_packages(exclude=('tests',)),
+        # If your package is a single module, use this instead of 'packages':
+        # py_modules=['mypackage'],
+
+        entry_points={
+            'console_scripts': ['okta-cli=oktacli:cli'],
+        },
+        install_requires=REQUIRED,
+        extras_require=EXTRAS,
+        include_package_data=True,
+        license='MIT',
+        classifiers=[
+            # Trove classifiers
+            # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: Implementation :: CPython',
+        ],
+        # $ setup.py publish support.
+        cmdclass={
+            'upload': UploadCommand,
+        },
 )
