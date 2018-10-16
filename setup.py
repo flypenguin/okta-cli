a23cfa6ee876f878e125883b005328fe7625d6b3
Axel Bock <mr.axel.bock@gmail.com>
2018-10-16 09:37:44 +0200

    merge python 3.6

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: Implementation :: CPython',
         ],
         # $ setup.py publish support.
