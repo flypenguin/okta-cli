90f1ede49d88ab1677d1f1a4addf251d2ea68319
Axel Bock <mr.axel.bock@gmail.com>
2019-04-14 13:18:26 +0200

    Bump version: 5.0.1 → 5.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-VERSION = "5.0.1"
+VERSION = "5.1.0"
 
 NAME = 'okta-cli'
 DESCRIPTION = 'An Okta command line interface for scripting and quickly performing routine tasks'
