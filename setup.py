1c0da731805ee8a44c1cfc2ba9c2b8381716fff8
Axel Bock <mr.axel.bock@gmail.com>
2020-07-07 18:49:32 +0200

    Bump version: 11.2.0 → 11.3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-VERSION = "11.2.0"
+VERSION = "11.3.0"
 
 NAME = 'okta-cli'
 DESCRIPTION = 'An Okta command line interface for scripting and quickly performing routine tasks'
