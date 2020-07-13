flypenguin/okta-cli

commit 1d1cd050f729493d93edfa94bdb5993e22b3a136

add nicer error handling and output

oktacli/cli.py
@@ -3,6 +3,7 @@ import sys
 import collections
 import csv
 import re
+import traceback
 from datetime import datetime as dt
 from functools import wraps
 from os.path import splitext, join, isdir
@@ -11,6 +12,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import click
 import yaml
+from requests import RequestException
 from dotted.collection import DottedDict, DottedCollection
 from requests.exceptions import HTTPError as RequestsHTTPError
 from openpyxl import load_workbook
@@ -22,6 +24,7 @@ from .api import filter_dicts
 from .api import get_config_file
 
 from .okta import REST
+from .okta import OktaAPIError
 from .exceptions import ExitException
 
 VERSION = "15.0.0"
@@ -115,10 +118,22 @@ def _command_wrapper(func):
         except ExitException as e:
             print("ERROR: {}".format(str(e)), file=sys.stderr)
             sys.exit(-1)
+        except RequestException as e:
+            print(f"COMMUNICATION_ERROR: {str(e)}", file=sys.stderr)
+            sys.exit(-1)
+        except OktaAPIError as e:
+            print(f"OKTA_API_ERROR: {e.error_code}: {str(e)}")
+            sys.exit(-3)
         except Exception as e:
-            print(f"SOMETHING REALLY BAD HAPPENED\n{str(type(e))}"
-                  f"!\nERROR: {e}",
-                  file=sys.stderr)
+            print("".join(traceback.format_exc()), file=sys.stderr)
+            print(f"""
+*****************************************************************************
+CRITICAL_ERROR: {str(type(e))}
+
+Please report at the issues page with details of what you did. Thank you!
+-> https://git.io/JJYqi
+*****************************************************************************
+""", file=sys.stderr)
             sys.exit(-2)
 
     return wrapper

oktacli/exceptions.py
@@ -1,2 +1,9 @@
-class ExitException(Exception):
+# OktaException is defined in okta.py :)
+
+
+class CLIException(Exception):
+    pass
+
+
+class ExitException(CLIException):
     pass

oktacli/okta.py
@@ -6,6 +6,15 @@ from urllib.parse import urljoin
 import requests
 
 
+class OktaAPIError(Exception):
+    def __init__(self, error_obj):
+        self.error_code = error_obj["errorCode"]
+        self.error_link = error_obj["errorLink"]
+        self.error_id = error_obj["errorId"]
+        self.error_causes = error_obj["errorCauses"]
+        super().__init__(error_obj["errorSummary"])
+
+
 class REST(enum.Enum):
     get = "get"
     put = "put"
@@ -56,8 +65,13 @@ class Okta:
             time.sleep(delay)
             # now try again
 
-        if rsp.status_code >= 400:
-            raise requests.HTTPError(json.dumps(rsp.json()))
+        rsp_code = rsp.status_code
+        if rsp_code >= 400:
+            if rsp_code < 500:
+                # Okta API error code
+                raise OktaAPIError(rsp.json())
+            else:
+                raise rsp.raise_for_statusss()
         return rsp
 
     def call_okta(self, path, method, *,

commit 65ef5c466f15e5bb10b4322109d44b8c2289b1f2

fix bug introduced with commit 1d1cd05
We catched the wrong exceptions in _okta_retrieve() - it is perfectly
normal for Okta to NOT find the given ID, since we we fall back to
string matching. But the error returned is no longer a RequestHTTPError
but an OktaAPIError.

Fixed.

oktacli/cli.py
@@ -230,7 +230,7 @@ def _okta_retrieve(thing, possible_id,
             # let's just return this if possible. also, no (!) "params" parameter
             # the params parameter contains the query, which we don't need here.
             return okta_manager.call_okta(f"/{thing}/{possible_id}", REST.get)
-        except RequestsHTTPError as e:
+        except OktaAPIError as e:
             pass
 
     # we're still here? so let's continue.