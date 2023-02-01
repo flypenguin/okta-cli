3fc0fcdf29cbcd47e279fc0f0dd9bb421293c5be
Axel Bock <ab@a3b3.de>
2023-02-01 01:44:55 +0100

    fix(setuptools): add missing __init__.py to dotted

--- setup.py
+++ setup.py
@@ -118,8 +118,6 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=("tests",)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
     entry_points={
         "console_scripts": ["okta-cli=oktacli:cli_main"],
     },
